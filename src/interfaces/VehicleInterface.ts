6e9fb0fd58c53e1c2f24bb8f7d927bc9fe1521f9
Amanda Zotelli <amzotelli@gmail.com>
2022-04-29 15:15:06 -0300

    :heavy_check_mark: cria interface Vehicle

--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,7 +1,17 @@
 import { z } from 'zod';
 
 const vehicleSchema = z.object({
-
+  model: z.string({ invalid_type_error: 'Model must be a string' })
+    .min(3, { message: 'Model must be 3 or more characters long' }),
+  year: z.number({ invalid_type_error: 'Year must be a number' })
+    .gte(1900, { message: 'Year must be 1900 or more' })
+    .lte(2022, { message: 'Year must be 2050 or less' }),
+  color: z.string({ invalid_type_error: 'Color must be a string' })
+    .min(3, { message: 'Color must be 3  or more characters long' }),
+  status: z.boolean({ invalid_type_error: 'Status must be a boolean' })
+    .optional(),
+  buyValue: z.number({ invalid_type_error: 'buyValue must be a number' })
+    .int({ message: 'buyValue must be an integer' }),
 });
 
 type Vehicle = z.infer<typeof vehicleSchema>;
