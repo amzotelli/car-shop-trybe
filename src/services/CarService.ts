83098e392e9dc858329f22d2768d9281a0cb0c45
Amanda Zotelli <amzotelli@gmail.com>
2022-04-29 16:17:48 -0300

    :heavy_check_mark: iniciando Car Service

---
+++ src/services/CarService.ts
@@ -0,0 +1,19 @@
+import { Car, CarType } from '../interfaces/CarInterface';
+import Service, { ServiceError } from './MongoService';
+import CarModel from '../models/CarModel';
+
+class CarService extends Service<Car> {
+  constructor(model = new CarModel()) {
+    super(model);
+  }
+
+  create = async (obj: Car): Promise<Car | ServiceError | null> => {
+    const parsed = CarType.safeParse(obj);
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+    return this.model.create(obj);
+  };
+}
+
+export default CarService;
