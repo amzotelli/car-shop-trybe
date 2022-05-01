2e3ad4a509d80e44b30e01ed1b975c134ad7c4dd
Amanda Zotelli <amzotelli@gmail.com>
2022-05-01 17:56:22 -0300

    :heavy_check_mark: teste Car Service

--- src/services/CarService.ts
+++ src/services/CarService.ts
@@ -14,28 +14,6 @@ class CarService extends Service<Car> {
     }
     return this.model.create(obj);
   };
-
-  read = async (): Promise<Car[]> => this.model.read();
-
-  readOne = async (id: string): Promise<Car | ServiceError | null> => {
-    const car = this.model.readOne(id);
-    if (!car) throw new Error('Car not found');
-    return car;
-  };
-
-  update = async (id: string, obj: Car): Promise<Car | ServiceError | null> => {
-    const parsed = CarType.safeParse(obj);
-    if (!parsed.success) {
-      return { error: parsed.error };
-    }
-    
-    const car = this.model.update(id, obj);
-    if (!car) throw new Error('Car not found');
-    return car;
-  };
-
-  delete = async (id: string): Promise<Car | ServiceError | null> => 
-    this.model.delete(id);
 }
-
+  
 export default CarService;
